7e5be21c8ba675c5c12fba432a7c1fb847f700d7
Griffon2012 <trifonov-ga@yandex.ru>
2022-12-14 12:48:39 +0300

    add code

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿void PrintArray(string[] array)
+{
+    int rows = array.Length;
+
+    for (int i = 0; i < rows; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+}
+
+int GetCountElemventByLength(string[] array)
+{
+    int nowIndex = 0;
+    int countString = 0;
+
+    while (nowIndex < array.Length)
+    {
+        if (array[nowIndex].Length <= 3)
+        {
+            countString++;
+        }
+        nowIndex++;
+    }
+
+    return countString;
+}
+
+string[] arrayStrings = { "123", "1234", "test", "run", "test2" };
+
+string[] arrayStringsLimitedByCondition = new string[GetCountElemventByLength(arrayStrings)];
+
+int position = 0;
+int nowIndex = 0;
+while (nowIndex < arrayStrings.Length)
+{
+    if (arrayStrings[nowIndex].Length <= 3)
+    {
+        arrayStringsLimitedByCondition[position] = arrayStrings[nowIndex];
+        position++;
+    }
+    nowIndex++;
+}
+Console.Write("Исходный массив: ");
+PrintArray(arrayStrings);
+Console.WriteLine();
+Console.Write("Результат: ");
+PrintArray(arrayStringsLimitedByCondition);
